552d1ffbed080c1d3ef08e8c91ee27b3a839a936
Marion <marion.zepf@gmail.com>
2013-07-26 17:22:56 +0200

    separate hand-exported and auto-exported TA samples

---
+++ samples-auto/basic-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    canvas = tw.canvas
+    logo = tw.lc
+
+    for i in range(logo.int(4.0)):
+        turtle.forward(100.0)
+        turtle.right(90.0)
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    start()
+    gtk.main()
+
+
