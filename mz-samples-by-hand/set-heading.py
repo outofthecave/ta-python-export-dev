449f6b7d0f37736e1a7703454dee2bc22162a0b8
Marion <marion.zepf@gmail.com>
2013-07-30 12:30:03 +0200

    set-heading (from mz-samples) uses talogo's execution engine

---
+++ mz-samples-by-hand/set-heading.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    canvas = tw.canvas
+    logo = tw.lc
+
+    turtle.forward(100.0)
+    turtle.set_heading(30.0)
+    turtle.forward(100.0)
+
+    yield True
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
+
+
