3be8fe1782a0c1101ed2c69c80a53044f4825865
Marion <marion.zepf@gmail.com>
2013-07-31 22:44:52 +0200

    sample for the 'clean' block

---
+++ mz-samples-auto/clean.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    turtle.forward(100.0)
+    tw.clear_plugins()
+    logo.prim_clear_helper()
+    canvas.clearscreen()
+    turtles.reset_turtles()
+    turtle.right(90.0)
+    turtle.forward(100.0)
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    start()
+    gtk.main()
+
+
