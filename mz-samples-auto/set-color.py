39f6df84853238dba92e708073f2daa1e0d7a301
Marion <marion.zepf@gmail.com>
2013-08-03 12:15:55 +0200

    add sample for the 'set color' block

---
+++ mz-samples-auto/set-color.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    turtle.forward(100.0)
+    turtle.set_color(60.0)
+    turtle.forward(100.0)
+    yield True
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
+
+
