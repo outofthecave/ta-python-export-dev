e388bac42202f2c862ce6012b8d5fb57b289096a
Marion <marion.zepf@gmail.com>
2013-08-03 14:31:35 +0200

    update everythin.ta to include all exportable blocks

--- mz-samples-auto/everything.py
+++ mz-samples-auto/everything.py
@@ -20,15 +20,24 @@ def start():
     logo.prim_clear_helper()
     canvas.clearscreen()
     turtles.reset_turtles()
-    canvas.fillscreen_with_gray(15.0, 80.0, 100.0)
-    for i in range(logo.int(6.0)):
+    turtle.set_pen_state(False)
+    turtle.forward((-turtle.get_shade()))
+    turtle.set_pen_state(True)
+    canvas.fillscreen_with_gray(15.0, 80.0, turtle.get_gray())
+    turtle.set_color(60.0)
+    turtle.set_shade(30.0)
+    turtle.set_pen_size(6.0)
+    turtle.start_fill()
+    for i in range(logo.int(turtle.get_pen_size())):
         turtle.forward(100.0)
-        turtle.right(-120.0)
-        turtle.forward(-100.0)
-        turtle.right(60.0)
+        turtle.right((-120.0))
+        turtle.forward((-100.0))
+        turtle.right(turtle.get_color())
         yield True
-    turtle.set_xy((0.0, 100.0))
+    turtle.stop_fill()
+    turtle.set_xy((0.0, 50.0))
     turtle.set_heading(210.0)
+    turtle.set_gray(0.0)
     for i in range(logo.int(2.0)):
         turtle.arc(180.0, 20.0)
         yield True
