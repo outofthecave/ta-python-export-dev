3c683bfa724cc302244023409bcc59c35577a66a
Marion <marion.zepf@gmail.com>
2013-08-03 13:26:41 +0200

    sample for the 'set pen size' block and the 'pen size' value block

---
+++ mz-samples-auto/set-pen-size.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    turtle.set_pen_size(30.0)
+    turtle.forward(100.0)
+    turtle.right(turtle.get_pen_size())
+    turtle.forward(100.0)
+    yield True
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
+
+
