d4b48c702bb4f6c129215238c86d519b4f3d088f
Marion <marion.zepf@gmail.com>
2013-08-03 13:38:57 +0200

    add sample for the 'start fill' and 'end fill' blocks

---
+++ mz-samples-auto/start-end-fill.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    turtle.start_fill()
+    for i in range(logo.int(2.0)):
+        turtle.forward(100.0)
+        turtle.right(90.0)
+        yield True
+    turtle.stop_fill()
+    for i in range(logo.int(2.0)):
+        turtle.forward(100.0)
+        turtle.right(90.0)
+        yield True
+    yield True
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
+
+
