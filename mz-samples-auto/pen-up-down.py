c78b155f48ae9f560b732b5a96b78616ce7d9aaa
Marion <marion.zepf@gmail.com>
2013-08-03 13:26:03 +0200

    sample for the 'pen up' and 'pen down' blocks

---
+++ mz-samples-auto/pen-up-down.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    for i in range(logo.int(8.0)):
+        turtle.set_pen_state(True)
+        turtle.forward(10.0)
+        turtle.set_pen_state(False)
+        turtle.forward(15.0)
+        yield True
+    yield True
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
+
+
