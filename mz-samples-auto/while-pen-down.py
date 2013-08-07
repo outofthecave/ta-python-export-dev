15294e94b401df68b6f77db2aded31eeae5c4a6c
Marion <marion.zepf@gmail.com>
2013-08-07 13:43:33 +0200

    mz-samples for the 'pen state', 'while', and 'until' blocks

---
+++ mz-samples-auto/while-pen-down.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    turtle.set_pen_state(True)
+    while turtle.get_pen_state():
+        turtle.forward(100.0)
+        turtle.right(90.0)
+        turtle.set_pen_state(False)
+        yield True
+    yield True
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
+
+
