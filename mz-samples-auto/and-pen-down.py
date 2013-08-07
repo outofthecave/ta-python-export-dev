274b201d3b9d109d971ba525e16518c4eadc25ee
Marion <marion.zepf@gmail.com>
2013-08-07 18:50:06 +0200

    mz-sample for the 'and' block

---
+++ mz-samples-auto/and-pen-down.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    while (not (turtle.get_pen_state() and (not turtle.get_pen_state()))):
+        turtle.forward(100.0)
+        turtle.right(90.0)
+        yield True
+    yield True
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
+
+
