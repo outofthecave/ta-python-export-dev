dd45499b521c6031f4d2fa90533d078936f9a0ba
Marion <marion.zepf@gmail.com>
2013-08-15 14:23:40 +0200

    mz-sample for the 'product' (multiplication) block

---
+++ mz-samples-auto/multiply.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from math import sqrt
+from random import uniform
+
+from pyexported.window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    turtle.forward(float(CONSTANTS["cyan"]) * 2.0)
+    turtle.right(90.0)
+    turtle.forward(40.0 * 5.0)
+    turtle.right(90.0)
+    turtle.forward(5.0 * float(CONSTANTS["yellow"]))
+    yield True
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
+
+
