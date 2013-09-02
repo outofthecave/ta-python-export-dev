144ce2956bdbd5e49384168e55479911d58d151f
Marion <marion.zepf@gmail.com>
2013-09-02 22:51:28 +0200

    add mz-sample for 'equals' block with colors 'red' and 'black'

---
+++ mz-samples-auto/red-equals-black.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from math import sqrt
+from random import uniform
+
+from pyexported.window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    if (CONSTANTS['red'] == CONSTANTS['black']):
+        turtle.right(90.0)
+    turtle.forward(100.0)
+    yield True
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
+
+
