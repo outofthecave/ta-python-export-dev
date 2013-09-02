4bd658f1f39b862143a5eac4bf4be6ead394e64b
Marion <marion.zepf@gmail.com>
2013-09-02 22:42:40 +0200

    add another mz-sample for the 'fillscreen' block

---
+++ mz-samples-auto/fillscreen-red.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from math import sqrt
+from random import uniform
+
+from pyexported.window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    canvas.fillscreen_with_gray(int(CONSTANTS['red']), 80.0, 100.0)
+    yield True
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
+
+
