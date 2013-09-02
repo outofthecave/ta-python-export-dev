ad52751f86c6bbd5ad9637968e00021925b0dce5
Marion <marion.zepf@gmail.com>
2013-09-02 22:41:48 +0200

    add 3 mz-samples for the 'and', 'or', and 'not' blocks

---
+++ mz-samples-auto/if-or.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from math import sqrt
+from random import uniform
+
+from pyexported.window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    if ((100.0 == 3.0) or (100.0 == 5.0)):
+        turtle.forward(100.0)
+    else:
+        turtle.forward((-100.0))
+    yield True
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
+
+
