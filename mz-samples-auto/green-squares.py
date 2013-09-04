f7760cb7b7e9017f0ebfcf10a1d7aa0eccb455a4
Marion <marion.zepf@gmail.com>
2013-09-04 12:08:46 +0200

    add mz-sample for the 'hat' and 'stack' blocks without boxes

---
+++ mz-samples-auto/green-squares.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+from math import sqrt
+from random import uniform
+
+from pyexported.window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    turtle.right((-90.0))
+    turtle.set_color(40.0)
+    turtle.set_shade(20.0)
+    turtle.set_gray(70.0)
+    logo.icall(ACTION[u'square'])
+    yield True
+    turtle.set_color(CONSTANTS['green'])
+    logo.icall(ACTION[u'square'])
+    yield True
+    turtle.set_color(40.0)
+    turtle.set_shade(int(CONSTANTS['yellow']))
+    logo.icall(ACTION[u'square'])
+    yield True
+    turtle.set_shade(20.0)
+    turtle.set_gray(int(CONSTANTS['blue']))
+    logo.icall(ACTION[u'square'])
+    yield True
+ACTION["start"] = start
+
+def square():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    turtle.start_fill()
+    for i in range(int(4.0)):
+        turtle.forward(50.0)
+        turtle.right(90.0)
+        yield True
+    turtle.stop_fill()
+    turtle.forward((-10.0))
+    turtle.right(90.0)
+    yield True
+ACTION["square"] = square
+
+
+
+
+if __name__ == '__main__':
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
+
+
