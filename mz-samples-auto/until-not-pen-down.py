8c09364bb6f3cc5846aae3e175214cc41ab955cd
Marion <marion.zepf@gmail.com>
2013-09-06 00:20:44 +0200

    add an mz-sample with the 'until' and 'not' blocks

---
+++ mz-samples-auto/until-not-pen-down.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from math import sqrt
+from random import uniform
+
+from pyexported.window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    turtle.set_pen_state(True)
+    while (not (not turtle.get_pen_state())):
+        turtle.forward(100.0)
+        turtle.right(90.0)
+        turtle.set_pen_state(False)
+        yield True
+    yield True
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
+
+
