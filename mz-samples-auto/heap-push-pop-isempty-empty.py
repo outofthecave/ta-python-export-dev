46763626b26cf9d51f23451b3858f7331094b9fd
Marion <marion.zepf@gmail.com>
2013-09-12 18:39:33 +0200

    add mz-sample for 'clear/ empty heap' block

---
+++ mz-samples-auto/heap-push-pop-isempty-empty.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from math import sqrt
+from random import uniform
+from time import (sleep, time)
+
+from pyexported.window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    BOX[u'i'] = 0.0
+    for i in range(int(4.0)):
+        logo.heap.append(BOX[u'i'])
+        if (convert(BOX[u'i'], TYPE_NUMBER) == 1.0):
+            logo.heap = []
+        BOX[u'i'] = convert(BOX[u'i'], TYPE_NUMBER) + 1.0
+        yield True
+    turtle.set_pen_state(False)
+    while (not (not logo.heap)):
+        logo.show(logo.heap.pop(), True)
+        turtle.forward((-50.0))
+        yield True
+    yield True
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    tw.lc.start_time = time()
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
+
+
