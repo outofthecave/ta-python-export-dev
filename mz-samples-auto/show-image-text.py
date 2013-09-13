070f12d369eb2f714ef917c9742936a4d5d08f51
Marion <marion.zepf@gmail.com>
2013-09-13 02:03:42 +0200

    mz-sample for the 'show' block with an image and some text

---
+++ mz-samples-auto/show-image-text.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from math import sqrt
+from random import uniform
+from time import (sleep, time)
+
+from pyexported.window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+
+def start():
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    logo.show(Media('media', u'/bigrepos/sugar-labs/turtleart/samples/images/Pythagoras.jpg'), True)
+    turtle.set_pen_state(False)
+    turtle.forward((-200.0))
+    logo.show(u'Pythagoras', True)
+    yield True
+ACTION["start"] = start
+
+
+
+
+if __name__ == '__main__':
+    tw.lc.start_time = time()
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
+
+
