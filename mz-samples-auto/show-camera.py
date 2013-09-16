4b84d6cd5a94a2f00a82942f6acb1361edf34110
Marion <marion.zepf@gmail.com>
2013-09-16 17:06:20 +0200

    add mz-sample for 'show' block with camera input

---
+++ mz-samples-auto/show-camera.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from time import *
+from random import uniform
+from math import *
+
+from pyexported.window_setup import *
+
+
+tw = get_tw()
+
+BOX = {}
+ACTION = {}
+
+
+def start():
+    tw.start_plugins()
+    turtle = tw.turtles.get_active_turtle()
+    turtles = tw.turtles
+    canvas = tw.canvas
+    logo = tw.lc
+
+    logo.show(Media('media', 'CAMERA'), True)
+    yield True
+ACTION["start"] = start
+
+
+if __name__ == '__main__':
+    tw.lc.start_time = time()
+    tw.lc.icall(start)
+    gobject.idle_add(tw.lc.doevalstep)
+    gtk.main()
